ad403d133f545fbdeba990da7b3fc2b4d9e46401
Riccardo Libanora <riccardo.libanora@gmail.com>
2022-12-28 22:56:48 +0100

    front-end

---
+++ scripts/maps.js
@@ -0,0 +1,77 @@
+let map;
+function initMap() {
+
+    var docelement = document.getElementById('map');
+    var attributes = {
+        center: { lat: 46.074779, lng: 11.121749 },
+        zoom: 12,
+        disableDefaultUI: true
+    }
+    map = new google.maps.Map(docelement, attributes);
+}
+
+function initAutocomp(index) {
+    var target = 'tappasearchbox'.concat(index);
+    var input = document.getElementById(target);
+
+    //deprecated
+    //new google.maps.places.Autocomplete(input);
+
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.addListener("bounds_changed", () => {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    let markers = [];
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach((marker) => {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach((place) => {
+            if (!place.geometry || !place.geometry.location) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            const icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+
+            // Create a marker for each place.
+            markers.push(
+                new google.maps.Marker({
+                    map,
+                    icon,
+                    title: place.name,
+                    position: place.geometry.location,
+                })
+            );
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
